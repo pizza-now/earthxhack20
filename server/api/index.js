1f80a85a3c61cd9c9301460c1eef1cd285f29a4d
Harmony Richman <hmrichman@gmail.com>
2020-04-24 15:08:34 -0700

    added user's POST route and routed to index file

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ module.exports = router
 
 router.use('/order', require('./order'))
 router.use('/contacts', require('./contacts'))
+router.use('/user', require('./users'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
