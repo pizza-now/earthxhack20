543c471467a7cc5388412e449d5b9f5de92ef624
Harmony Richman <hmrichman@gmail.com>
2020-04-24 16:56:48 -0700

    added alert messages for small, medium, and large

---
+++ alerts/large.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Large = (props) => {
+      const { 
+            userName,
+            userAddress,
+            userPhone,
+            keyword,
+            contactName,
+             
+
+      } 
+
+      return (
+            <div>
+                  Hi {contactName},
+                  <br/>
+                  You are receiving this alert from {userName} via the Pizza Now app because XXX. They are currently located at {userAddress} and you can keep track of their location using *geolocation*. {userName} is in danger and requires immediate help. Please contact the authorities and make your way to {userName}'s location.
+                  <br/>
+                  Stay Safe,
+                  <br/>
+                  The Pizza Now Team
+                
+            </div>
+      )
+}
+
+export default connect(mapState)(Large)
---
+++ alerts/small.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+const Small = (props) => {
+      const { 
+            userName,
+            userAddress,
+            userPhone,
+            keyword,
+            contactName,
+             
+
+      } 
+
+      return (
+            <div>
+                  Hi {contactName},
+                  <br/>
+                  You are receiving this alert from {userName} via the Pizza Now app because XXX. They are currently located at {userAddress} and you can keep track of their location using *geolocation*. {userName} would like you to check up on them via call or text immediately. You can reach them at {userPhone}. 
+                  <br/>
+                  {userName}'s safety word is <i>'{keyword}'</i>. If they say <i>'{keyword}'</i> when you speak to them, it means they are in danger and need you to <strong>contact the authorities immediately.</strong>
+                  <br/>
+                  Stay Safe,
+                  <br/>
+                  The Pizza Now Team
+                
+            </div>
+      )
+}
+
+export default connect(mapState)(Small)
