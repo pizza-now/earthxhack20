f24afca82194f8dcb06d6d1d34ea5c758f6c2789
Harmony Richman <hmrichman@gmail.com>
2020-04-24 09:51:37 -0700

    created user model

---
+++ server/db/models/users.js
@@ -0,0 +1,55 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const User = db.define('user', {
+  fullName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
+  },
+  address: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+              notEmpty: true,
+        }
+  },
+  keyword: {
+      type: Sequelize.STRING,
+      allowNull: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    // Making `.password` act like a func hides it when serializing to JSON.
+    // This is a hack to get around Sequelize's lack of a "private" option.
+    get() {
+      return () => this.getDataValue('password')
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    // Making `.salt` act like a function hides it when serializing to JSON.
+    // This is a hack to get around Sequelize's lack of a "private" option.
+    get() {
+      return () => this.getDataValue('salt')
+    }
+  },
+  googleId: {
+    type: Sequelize.STRING
+  },
+})
+
+module.exports = User
