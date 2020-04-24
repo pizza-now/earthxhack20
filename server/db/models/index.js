f28ea43f099434b69249ed3700cf41ee94034310
Tani314 <tanitabassum299@gmail.com>
2020-04-24 15:23:01 -0400

    orders added

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -11,13 +11,13 @@ Contact.belongsTo(User)
 User.hasMany(Contact)
 Order.belongsTo(User) //order belongs to one user with userId
 User.hasMany(Order)
-User.afterCreate(user => {
-  //create the User's cart
-  Order.create({
-    dateOfSubmission:new Date(),
-    userId: user.id
-  })
-})
+// User.afterCreate(user => {
+//   //create the User's cart
+//   Order.create({
+//     dateOfSubmission:new Date(),
+//     userId: user.id
+//   })
+// })
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
