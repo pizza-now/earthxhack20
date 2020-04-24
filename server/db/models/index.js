19bb3d38f24c7dea95407f8ab5b1ee848598a57f
Tani314 <tanitabassum299@gmail.com>
2020-04-24 14:28:47 -0400

    orders added

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./users')
 const Contact = require('./contacts')
-
+const Order = require('./orders')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -9,7 +9,15 @@ const Contact = require('./contacts')
  */
 Contact.belongsTo(User)
 User.hasMany(Contact)
-
+Order.belongsTo(User) //order belongs to one user with userId
+User.hasMany(Order)
+User.afterCreate(user => {
+  //create the User's cart
+  Order.create({
+    dateOfSubmission:new Date(),
+    userId: user.id
+  })
+})
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -18,5 +26,6 @@ User.hasMany(Contact)
  */
 module.exports = {
   User,
-  Contact
+  Contact,
+  Order
 }
