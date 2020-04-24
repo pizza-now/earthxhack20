195fcd56aad326fee01625fc242a820054d6ce96
Natalia Gabuaeva <ngabuaeva@gmail.com>
2020-04-24 18:28:28 -0400

    splash-page, store

--- client/components/guest.js
+++ client/components/guest.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
-class Guest extends React.Component{
-  render() {
-    return(
-      <div>
-        <h1>QUICK CHECKOUT</h1>
-      </div>
-    )
-  }
+const Guest = () => {
+  return (
+    <div>
+      <h1>QUICK CHECKOUT</h1>
+    </div>
+  )
 }
 
 export default Guest
--- client/routes.js
+++ client/routes.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import Guest from "./components/guest"
-import SplashPage from './components/splash-page'
-
+import { withRouter, Route, Switch } from 'react-router-dom'
+import { SplashPage, Login, Signup, Guest } from './components'
+import { me } from './store';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
 
 class Routes extends Component {
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
+
   render() {
     return (
       <Switch>
         {/* Routes placed here are available for everyone */}
-        {<Route path="/guest" component={Guest} />}
+        <Route exact path="/" component={SplashPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/guest" component={Guest} />
 
         {/* Displays the SplashPage component as a fallback */}
         <Route component={SplashPage} />
@@ -18,4 +26,18 @@ class Routes extends Component {
   }
 }
 
-export default Routes
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
+};
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(null, mapDispatch)(Routes));
+
+Routes.propTypes = {
+  loadInitialData: PropTypes.func.isRequired,
+};
