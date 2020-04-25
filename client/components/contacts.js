f92e4bb6406c82a077149da9fb27e65a953538f0
Natalia Gabuaeva <ngabuaeva@gmail.com>
2020-04-25 14:58:37 -0400

    update contact

--- client/components/contacts.js
+++ client/components/contacts.js
@@ -1,38 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { getContacts, deleteContact } from '../store/contacts'
+import { getContacts } from '../store/contacts'
 import AddContactForm from './addContactForm'
-import UpdateContactForm from './updateContactForm'
-import { Button } from '@material-ui/core';
+import SingleContact from './singleContact'
 
 const Contacts = () => {
   const contacts = useSelector(state => state.contacts)
   const dispatch = useDispatch()
   const [form, setForm] = useState('')
-  const [form2, setForm2] = useState('')
+  const setAddForm = () => !form ? setForm(<AddContactForm />) : setForm('')
+
   useEffect(() => {
     dispatch(getContacts())
   }, [])
-
-  const setUpdateForm = () => !form2 ? setForm2(<UpdateContactForm />) : setForm2('')
-
-  const setAddForm = () => !form ? setForm(<AddContactForm />) : setForm('')
 
   return (
     <div>
       <h1>People you've referred:</h1>
       {contacts.length ? (
         contacts.map(contact => (
-          <div key={contact.id}>
-            <p>Name: {contact.fullName}</p>
-            <p>Email: {contact.email}</p>
-            <p>Phone number: {contact.phoneNumber}</p>
-
-            <button onClick={setUpdateForm} className='button'>Update</button>
-            {form2 ? <UpdateContactForm contact={contact} setForm={setUpdateForm} /> : ''}
-
-            <button onClick={() => dispatch(deleteContact(contact.id))}>Remove referral</button>
-          </div>
+          <SingleContact key={contact.id} contact={contact} />
         ))
       ) : (
           <div>No friends referred yet </div>
