93437e087ff3a730eb8108b1e800944de35a24f4
Tani314 <tanitabassum299@gmail.com>
2020-04-25 01:04:05 -0400

    home page

--- client/components/selectSize.js
+++ client/components/selectSize.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { TextField } from '@material-ui/core';
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4caf50',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
 
 export default function SelectSize() {
   return (
     <div className='pizza'>
+    <ThemeProvider theme={theme}>
+    <div>
       <Button variant="contained" color="primary">
         Small
       </Button>
+      </div>
+      <div>
       <Button variant="contained" color="primary">
         Medium
       </Button>
+      </div>
+      <div>
       <Button variant="contained" color="primary">
         Large
       </Button>
+      </div>
+      </ThemeProvider>
+      <div>
+      <TextField id="filled-basic" label="Customize Your Pizza" variant="filled" />
+      </div>
     </div>
   );
 }
