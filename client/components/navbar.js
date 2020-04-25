9a1197464cbee67dbd9d0b7358b2ebc74f2bbd43
Natalia Gabuaeva <ngabuaeva@gmail.com>
2020-04-25 15:48:30 -0400

    minor fix

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,39 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => {
+const Navbar = ({ handleClick, isLoggedIn }) => {
+  console.log('isLoggedIn:', isLoggedIn)
   return (
     <div className="nav-container">
       <Link to='/'>
         <div className="nav-logo">
-          <img src={'/assets/pizza-now.png'} alt="logo" className="logo"/>
+          <img src={'/assets/pizza-now.png'} alt="logo" className="logo" />
         </div>
       </Link>
       <nav className="navbar-links">
-      {isLoggedIn ? (
-        <div>
-          <Link to="/home">Home</Link>
-          <Link to="/referrals">Refer A Friend</Link>
-          <a href="#" onClick={handleClick}>
-          Logout
-          </a>
-        </div>
-        ):(
+        {isLoggedIn ? (
           <div>
-        <Link to="/guest">Guest Checkout</Link>
-        </div>)}
+            <Link to="/home">Home</Link>
+            <Link to="/referrals">Refer A Friend</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+          </a>
+          </div>
+        ) : (
+            <div>
+              <Link to="/guest">Guest Checkout</Link>
+            </div>)}
       </nav>
     </div>
   )
 }
 
 const mapState = state => {
-  console.log('STATE',state)
   return {
-    isLoggedIn: !!state.user
+    isLoggedIn: !!state.user.id
   }
 }
 
