8cd50598a41df4b917675ae0146f4461b0bf6143
Tani314 <tanitabassum299@gmail.com>
2020-04-25 23:30:27 -0400

    guest checkout error fixed

--- client/components/guest.js
+++ client/components/guest.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SelectSize from './selectSize'
 import AddContactForm from "./addContactForm"
 
 const Guest = () => {
+  const [setForm] = useState('')
+  const setAddForm = () => setForm(<AddContactForm />)
   return (
     <div className="guest-page">
       <h2 id="guest-message">Quick cheese pizza is just a click away! Refer a friend to get a free cheese pizza of your choice size!</h2>
-      <AddContactForm/>
+      <AddContactForm user={2020} setForm={setAddForm}/>
       <SelectSize/>
     </div>
   )
