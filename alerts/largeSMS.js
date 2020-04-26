ba7509fd15bc754e6c850ae55b5ebce6b75f99b0
Harmony Richman <hmrichman@gmail.com>
2020-04-26 15:07:27 -0700

    removed extra files

--- alerts/largeSMS.js
+++ alerts/largeSMS.js
@@ -5,7 +5,7 @@ const sendLargeSMS = (user, geolocation, contacts) => {
   contacts.forEach(contact => {
     messagebird.messages.create({
       originator: 'Pizza Now',
-      recipients: contact.phoneNumber,
+      recipients:[ `${contact.phoneNumber}` ],
       body: `Hi ${contact.fullName}, You are receiving this alert from ${user.fullName} via the Pizza Now app because of a domestic violence situation that requires immediate action. ${user.fullName} is in danger and requires immediate help. ${user.fullName} iscurrently located at ${user.address} and you can keep track of their location using ${geolocation}. Please contact the authorities and make your way to ${user.fullName}'s location. Stay Safe - The Pizza Now Team`
     },
       function (err, response) {
