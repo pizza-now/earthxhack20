cbd841c806f5815bdd7cf9c7c9c580a182b5204c
Joy Wang <w.joy5015@gmail.com>
2020-05-05 21:46:14 -0400

    added hyperlink to contact

--- client/components/beta-banner.js
+++ client/components/beta-banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BetaBanner() {
   return(
     <div className ="attention">
-        <span> <b>Attention:</b> This app is currently is beta and should not be used outside of proof of concept purposes. Please contact the developers for any inquiries.</span>
+        <span> <b>Attention:</b> This app is currently is beta and should not be used outside of proof of concept purposes. Please <a href="https://github.com/pizza-now/earthxhack20">contact</a> the developers for any inquiries.</span>
     </div>
   )
 }
